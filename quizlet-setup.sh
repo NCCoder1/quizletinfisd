ae2b00e517e4e997950f9969b077ac76652cae40
syoung-quizlet <sean.young@quizlet.com>
2020-03-03 13:33:02 -0800

    add script

---
+++ quizlet-setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+CURRENT_USER=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+echo -e "--- Commandline Tools ---\n" | tee $LOG_FILE
+
+os=$(sw_vers -productVersion | awk -F. '{print $1 "." $2}')
+
+# string comparison
+if [[ "$os" == 10.15 ]]; then
+	echo "macOS Catalina"
+    if softwareupdate --history | grep --silent "Command Line Tools.*"; then
+		echo 'Command-line tools already installed.'
+	else
+		echo ""
+		echo '##### Installing Command-line tools...'
+    	in_progress=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+    	touch ${in_progress}
+    	product=$(softwareupdate -l | grep -B 1 -E 'Command Line Tools' | awk -F'*' '/^ *\\*/ {print $2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1)
+    	softwareupdate --verbose --install "${product}" || echo 'Installation failed.' 1>&2 && rm ${in_progress} && echo 'Command-line tools installed.'
+	fi
+
+
+elif [[ "$os" == 10.14 ]]; then
+	echo "macOS High Sierra"
+    if softwareupdate --history | grep --silent "Command Line Tools.*${os}"; then
+		echo 'Command-line tools already installed.'
+	else
+		echo ""
+		echo '##### Installing Command-line tools...'
+    	in_progress=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+    	touch ${in_progress}
+    	product=$(softwareupdate --list | awk "/\* Command Line.*${os}/ { sub(/^   \* /, \"\"); print }")
+    	softwareupdate --verbose --install "${product}" || echo 'Installation failed.' 1>&2 && rm ${in_progress} && echo 'Command-line tools installed.'
+	fi
+else
+	echo "Mac OS X 10.13 or earlier"
+    if softwareupdate --history | grep --silent "Command Line Tools.*${os}"; then
+		echo 'Command-line tools already installed.'
+	else
+		echo ""
+		echo '##### Installing Command-line tools...'
+    	in_progress=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+    	touch ${in_progress}
+    	product=$(softwareupdate --list | awk "/\* Command Line.*${os}/ { sub(/^   \* /, \"\"); print }")
+    	softwareupdate --verbose --install "${product}" || echo 'Installation failed.' 1>&2 && rm ${in_progress} && echo 'Command-line tools installed.'
+	fi
+fi
+
+sudo mkdir -p /opt/projects
+sudo chown -R $CURRENT_USER /opt/projects
+ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
+sudo ssh-keyscan -t rsa github.com > /etc/ssh/ssh_known_hosts
+echo "##### quizlet-web..."
+cd /opt/projects
+git clone git@github.com:quizlet/quizlet-web.git quizlet
+
+echo "##### quizlet-puppet..."
+git clone git@github.com:quizlet/quizlet-puppet.git
+
+
+echo "##### quizlet-workstation..."
+git clone git@github.com:quizlet/quizlet-workstation.git
+
+chown -R $CURRENT_USER /opt/projects
