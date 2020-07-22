d0212510fe5bb44fc67db67b626f2ae1468ba0b8
syoung-quizlet <sean.young@quizlet.com>
2020-07-21 14:39:18 -1000

    misc changes

--- quizlet-setup.sh
+++ quizlet-setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 CURRENT_USER=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
-
+echo "######################################"
+echo "####                              ####"
 echo "#### Preparing Laptop for Quizlet ####"
+echo "####                              ####"
+echo "######################################"
+
+sleep 3
 
 echo "--- Commandline Tools ---\n"
 os=$(sw_vers -productVersion | awk -F. '{print $1 "." $2}')
@@ -52,20 +57,44 @@ sudo ssh-keyscan -t rsa github.com > ~/.ssh/ssh_known_hosts
 
 # Check if SSH is authorized to Github
 if ssh -q git@github.com; [ $? -eq 255 ]; then
-   echo "We were not able to successfully connect to Github.  Please fix and rerun script."
+	echo "We were not able to successfully connect to Github.  Please fix and rerun script."
 else
-   # successfully authenticated
-   echo "##### quizlet-web..."
-   cd /opt/projects
-   git clone git@github.com:quizlet/quizlet-web.git quizlet
+	# successfully authenticated
+	if [ -d "/opt/projects/quizlet" ]; then
+		echo ""
+		echo "##### Updating quizlet-web..."
+		cd /opt/projects/quizlet
+		git fetch && git reset --hard origin/master && git checkout master && git pull origin master && git submodule sync && git submodule update && git clean -ffd
+	else
+		echo ""
+		echo "##### Downloading quizlet-web..."
+	    cd /opt/projects
+	    git clone git@github.com:quizlet/quizlet-web.git quizlet
+	fi
 
-   echo "##### quizlet-puppet..."
-   git clone git@github.com:quizlet/quizlet-puppet.git
+	if [ -d "/opt/projects/quizlet-puppet" ]; then
+		echo ""
+		echo "##### Updating quizlet-puppet..."
+		cd /opt/projects/quizlet-puppet
+		git fetch && git reset --hard origin/master && git checkout master && git pull origin master && git submodule sync && git submodule update && git clean -ffd
+	else
+		echo ""
+		echo "##### Downloading quizlet-puppet..."
+		cd /opt/projects/
+		git clone git@github.com:quizlet/quizlet-puppet.git
+	fi
 
-
-   echo "##### quizlet-workstation..."
-   git clone git@github.com:quizlet/quizlet-workstation.git
-
+	if [ -d "/opt/projects/quizlet-workstation" ]; then
+		echo ""
+		echo "##### Updating quizlet-workstation..."
+		cd /opt/projects/quizlet-workstation
+		git fetch && git reset --hard origin/master && git checkout master && git pull origin master && git submodule sync && git submodule update && git clean -ffd
+	else
+		echo ""
+		echo "##### Downloading quizlet-workstation..."
+		cd /opt/projects/
+		git clone git@github.com:quizlet/quizlet-workstation.git
+	fi
    chown -R $CURRENT_USER /opt/projects
 fi
 
